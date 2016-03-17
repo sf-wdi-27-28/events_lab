030ac98ac4fa26e09aea0f8ca4c548c350445f7c
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-17 15:55:12 -0700

    problem 1 solved

--- submit/js/app.js
+++ submit/js/app.js
@@ -1,7 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function() {
-
+  $(".input").on("click", function stopClick (event){
+       return false;
+   })
   $("#time").text(Date.now());
 
 });

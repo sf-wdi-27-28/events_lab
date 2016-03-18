0efd1136348980db2d88c6a02e57663c527ecdd1
Nathan L <adamnate@gmail.com>
2016-03-17 18:02:28 -0700

    click and keypress

--- change/js/app.js
+++ change/js/app.js
@@ -3,5 +3,9 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function() {
 
   $("#time").text(Date.now());
+  $("form").on("click", function handleClick(event){
+    event.preventDefault();
+    // more code down here
+});
 
 });

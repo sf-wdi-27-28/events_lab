44b8be0bd5da7423a0a5d8150d74156dfd67a1ab
altonelli <altonelli14@gmail.com>
2016-03-17 19:27:45 -0700

    Completed assignment, some refactoring could be done.

--- submit/js/app.js
+++ submit/js/app.js
@@ -1,7 +1,12 @@
 console.log("Sanity Check: JS is working!");
 
+
 $(document).ready(function() {
-
+  $("input").click(function(event) {
+    event.preventDefault();
+  });
+  $('input').click(function(){
+    $("body").append('<iframe width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>');
+  });
   $("#time").text(Date.now());
-
 });

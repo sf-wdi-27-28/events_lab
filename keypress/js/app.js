2aee1bef474a928ae78be32834080206a90c7248
ahza64 <ahza64@yahoo.com>
2016-03-17 20:26:36 -0700

    homework with T/A help

--- keypress/js/app.js
+++ keypress/js/app.js
@@ -1,5 +1,21 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function() {
-
+  var startTime;
+  var stopTime;
+  var running = false;
+  var totalTime;
+  $(window).on('keypress', function boom(event){
+    if(event.keyCode === 32){
+      if(running){
+        endTime = Date.now();
+        totalTime = ((endTime - startTime)/1000);
+        running = false;
+        $('#total-time').text(totalTime);
+      }else{
+        startTime = Date.now();
+        running = true;
+      }
+    }
+  });
 });
--- ready/js/app.js
+++ ready/js/app.js
@@ -1,9 +1,7 @@
-/*
-  What needs to change so that the page says "Go!"?
-*/
+
 
 $(document).ready(function() {
-
+  $("h1").text("Go!");
   console.log("The page says:", $("h1").text());
   if ($("h1").text() === "Go!") {
     console.log("Success!");

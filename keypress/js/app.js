dc42f629b4cac4d282465ad3095e834c8914df51
srhbnnstr <srhbnnstr@gmail.com>
2016-03-17 22:15:08 -0700

    keypress

--- keypress/js/app.js
+++ keypress/js/app.js
@@ -2,4 +2,21 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function() {
 
+var startTime;
+var endTime;
+var Total;
+
+$(window).on("keypress", function (e)) {
+
+	if (e.keyCode ==== 32 && startTime) {
+		endTime = Date.now();
+		total = (endTime - startTime) / 1000;
+		  $("#total-time").text(total + " seconds");
+    } else {
+      startTime = Date.now();
+    }
+
+  });
+
 });
+

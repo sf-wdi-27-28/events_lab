867872d47432fe41b6c5f9cced7630f94cd555f3
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-03-18 02:19:45 -0700

    Final HW events_lab

--- click/js/app.js
+++ click/js/app.js
@@ -2,4 +2,21 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function() {
 
+  $(".span").on("click", function (events){
+
+    $("span").on("click", function handleClick(event) {
+    var itemText = "You Clicked: " + $(this).text() + " at " + Date.now();
+    $("ul").append("<li>" + itemText + "</li>");
+  });
+  })
 });
+
+//my original code
+$(".span").on("click", function (events){
+  $("ul").append("events");
+}
+
+/*Had to look at the solutions for this one. Couldn't figure out 
+how to use the <li> tags. Still trying to find out truly how to
+use "This"
+*/

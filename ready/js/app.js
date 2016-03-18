bda1194855d3ceedc3e26b7738bc3e81c5e8f75f
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-03-17 20:08:23 -0700

    successfully completed first exercise, and received help/relied on solution for 4 and 5! Attempting to do other exercises after submitting this...

--- ready/js/app.js
+++ ready/js/app.js
@@ -2,12 +2,4 @@
   What needs to change so that the page says "Go!"?
 */
 
-$(document).ready(function() {
-
-  console.log("The page says:", $("h1").text());
-  if ($("h1").text() === "Go!") {
-    console.log("Success!");
-  } else {
-    console.log("Can you make it say \"Go!\"?");
-  }
-});
+$("h1").text("Go!!");

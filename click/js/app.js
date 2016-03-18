86d1a2e92ead8e9de4cba058b7e219dfe002857d
mbd-s <michaelbsilverberg@gmail.com>
2016-03-17 21:59:07 -0700

    hwk solution and progress

--- click/js/app.js
+++ click/js/app.js
@@ -3,3 +3,12 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function() {
 
 });
+
+$("span").on("click", function handleClick(event) {
+$("ul").append("<li> "+ $(this).text() + " at: " + event.timeStamp +"</li>");
+});
+
+// });
+// $("span").on("click", function trackClicks(event){
+// $(this).clone().add("li").appendTo("ul");
+// });
--- keypress/js/app.js
+++ keypress/js/app.js
@@ -3,3 +3,9 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function() {
 
 });
+
+$(document).on("keypress", function stopAndStart(event) {
+if (event.which === 32) {
+console.log(event.timeStamp);
+}
+});

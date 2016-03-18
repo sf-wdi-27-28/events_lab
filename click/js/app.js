d595479f5c6b01b05dddf5b7ebd3525a700d45dc
srhbnnstr <srhbnnstr@gmail.com>
2016-03-17 21:47:08 -0700

    click

--- click/js/app.js
+++ click/js/app.js
@@ -1,5 +1,11 @@
 console.log("Sanity Check: JS is working!");
 
+
 $(document).ready(function() {
+
+  $("span").on("click", function handleClick(event) {
+    var clickText = "You Clicked: " + $(this).text() + " at " + Date.now();
+    $("ul").append("<li>" + clickText + "</li>");
+  });
 
 });

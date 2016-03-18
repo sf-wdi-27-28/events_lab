9e643cc4adb148d6ba1d209bd2ebd01726afdd5f
smicl11 <bryan.smith094@gmail.com>
2016-03-17 17:47:27 -0700

    almost there with keypress

--- keypress/js/app.js
+++ keypress/js/app.js
@@ -2,4 +2,20 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function() {
 
+  var begin;
+  var end;
+  var ellapsedTime;
+
+  $(window).on("keypress", startOrStopTime(event) {
+
+    if (beign) {
+      end = Date.now();
+      ellapsedTime = (end - begin) / 1000;
+      $("#total-time").text(ellapsedTime);
+    } else {
+      begin = Date.now();
+    }
+
+  });
+
 });

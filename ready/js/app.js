6336f645adb22fb67210bca118bea5afc2a941ad
Ameer Brown <akeithbrown@gmail.com>
2016-03-19 22:57:06 -0700

    Events Labb

--- ready/js/app.js
+++ ready/js/app.js
@@ -5,8 +5,11 @@
 $(document).ready(function() {
 
   console.log("The page says:", $("h1").text());
-  if ($("h1").text() === "Go!") {
-    console.log("Success!");
+  if ($("h1").text() === "Ready?") {
+    console.log("Go!");
+    alert("Hey Guys! Sure you're ready? Ok, click and be on your way!");
+    //added "Go!"" into the .text() to set the the text for "h1"
+    $("h1").text("Go!");
   } else {
     console.log("Can you make it say \"Go!\"?");
   }
